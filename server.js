c20497c416353fa37d2cca639126cb0ed5e2bfb5
Aleksandra Joachimiak <joachim07@poczta.onet.pl>
2020-12-29 23:24:48 +0100

    Add layouts/, Modify server.js & views/ (usehandlebars)

--- server.js
+++ server.js
@@ -1,35 +1,48 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-  res.show('home.html');
+  res.render('home');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
 });
 
 app.get('/home', (req, res) => {
-  res.show('home.html');
+  res.render('home');
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about', { layout: 'dark' });
 });
 
 app.use('/user', (req, res) => {
-  res.show('forbidden.html')
+  res.render('forbidden')
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/info', (req, res) => {
+  res.render('info', { layout: 'dark' });
+});
+
+app.get('/history', (req, res) => {
+  res.render('history');
 });
 
 app.use((req, res) => {
-  res.status(404).show('404.html')
+  res.status(404).render('404')
 });
 
 app.listen(8000, () => {
